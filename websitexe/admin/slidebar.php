55ffaa5a727d982a60186551f07ffa9bf1c11b56
hiu1412 <144113591+hiu1412@users.noreply.github.com>
2025-03-05 14:46:19 +0700

    add more sidebar tabtab

---
+++ websitexe/admin/slidebar.php
@@ -0,0 +1,9 @@
+<div class="fixed w-64 h-screen bg-gray-800 text-white p-5">
+    <h2 class="text-xl font-bold mb-5">Admin Panel</h2>
+    <ul>
+        <li class="mb-3"><a href="index.php" class="hover:text-gray-400">Dashboard</a></li>
+        <li class="mb-3"><a href="./manager/users.php" class="hover:text-gray-400">Quản lý User</a></li>
+        <li class="mb-3"><a href="./car/cars.php" class="hover:text-gray-400">Quản lý Xe</a></li>
+        <li class="mb-3"><a href="./car/cars.php" class="hover:text-gray-400">Quản lý Xe</a></li>
+    </ul>
+</div>
